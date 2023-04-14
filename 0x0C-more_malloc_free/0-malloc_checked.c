ca01f3ce6afe53bbdcf31fde7f81124c017dca55
sammieagb <gliddencop@gmail.com>
2023-04-13 19:01:51 -0700

    thg

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,16 +5,11 @@
 
 
 /**
-
-* *malloc_checked - allocates memory using malloc
-
-* @b: number of bytes to allocate
-
-*
-
-* Return: a pointer to the allocated memory
-
-*/
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 
 void *malloc_checked(unsigned int b)
 
